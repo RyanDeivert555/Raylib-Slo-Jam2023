cd625aa562715f3730674fd78abc0fea118e3b60
Ryan Deivert <rdeivert@icloud.com>
2023-12-20 18:55:43 -0800

    added web support

--- source/main.cpp
+++ source/main.cpp
@@ -2,20 +2,33 @@
 #include "../include/world.h"
 #include "raylib.h"
 
+#ifdef __EMSCRIPTEN__
+	#include <emscripten/emscripten.h>
+#endif
+
+void MainLoop() {
+    World::Update();
+    BeginDrawing();
+    ClearBackground(WHITE);
+    World::Draw();
+    EndDrawing();
+}
+
 int main() {
     SetTraceLogLevel(LOG_WARNING);
     InitWindow(windowWidth, windowHeight, "GameJam2023");
-    SetTargetFPS(60);
     
     Sprite::Init();
 
-    while (!WindowShouldClose()) {
-        World::Update();
-        BeginDrawing();
-        ClearBackground(WHITE);
-        World::Draw();
-        EndDrawing();
-    }
+    #ifdef __EMSCRIPTEN__
+        emscripten_set_main_loop(MainLoop, 60, 1);
+    #else
+        SetTargetFPS(60);
+        while (!WindowShouldClose()) {
+            MainLoop();
+        }
+    #endif
+
     Sprite::UnloadTextures();
     CloseWindow();
     
